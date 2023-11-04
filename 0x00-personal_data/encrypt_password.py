051502a62ec5b95f6d7a121b54f72671578bd8ec
R-Owino <sayeakinyi@gmail.com>
2023-11-04 23:06:58 +0300

    Hashed password validation

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,11 @@ def hash_password(password: str) -> bytes:
     hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Function that validates that the provided passwords matches
+    the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
