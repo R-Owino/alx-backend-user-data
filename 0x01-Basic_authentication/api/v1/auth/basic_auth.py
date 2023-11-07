f80ef61dc3b556bdb4e68801d081a9d876a18000
R-Owino <sayeakinyi@gmail.com>
2023-11-07 22:17:44 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains a class BasicAuth that inherits from Auth.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class """
+
+    def __init__(self):
+        """ Initialize BasicAuth """
+        pass
