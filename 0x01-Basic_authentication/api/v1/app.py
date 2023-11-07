89c226e3d8ca4e1707d16e22d22fd122df056240
R-Owino <sayeakinyi@gmail.com>
2023-11-07 22:17:30 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ auth = None
 if os.getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif os.getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -42,19 +45,23 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request_func() -> str:
-    """ Before request handler
+    """ Before request handler, filters which request need authentication
     """
     if auth is None:
         return
+    
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
+    
     if not auth.require_auth(request.path, excluded_paths):
         return
+    
     if auth.authorization_header(request) is None:
-        abort(401)
+        abort(401, description="Unauthorized")
+
     if auth.current_user(request) is None:
-        abort(403)
+        abort(403, description="Forbidden")
 
 
 if __name__ == "__main__":
