15a8abb955c055e33268fa1782e81dfd8ab19dca
R-Owino <sayeakinyi@gmail.com>
2023-11-07 21:42:27 +0300

    Define routes that don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,6 +13,9 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
+        Args:
+            - path: path to check
+            - excluded_paths: list of paths that do not need authentication
         Returns:
           - True if path is not in the list of strings excluded_paths
         """
