9a64b48dbe8748cdc699ca6c75d47068f237543d
R-Owino <sayeakinyi@gmail.com>
2023-11-16 00:51:23 +0300

    Chore: Get session ID by user email

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,3 +47,16 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Finds a user by email, generates a new UUID and store it in the
+        database as the user’s session_id, then return the session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
