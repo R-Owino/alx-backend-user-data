2bf0bacee134448c309c6ef2904ead743269f914
R-Owino <sayeakinyi@gmail.com>
2023-11-16 09:24:01 +0300

    Chore: Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,15 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except ValueError:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generates a UUID and updates the user’s reset_token database field
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
