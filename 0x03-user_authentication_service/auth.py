3c62db63c7159e2227caeb361886aa257370e63a
R-Owino <sayeakinyi@gmail.com>
2023-11-16 09:10:44 +0300

    Chore: Destroy a session using Used ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,12 @@ class Auth:
             return user.email
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Updates the corresponding user’s session ID to None
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            pass
